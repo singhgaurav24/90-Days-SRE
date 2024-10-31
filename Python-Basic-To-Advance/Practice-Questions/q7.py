5d0ab7116618b1e91df3be670ae12e7275a6c833
Gaurav Kumar Singh <55310399+singhgaurav24@users.noreply.github.com>
2024-10-31 18:31:39 +0000

    python

---
+++ Python-Basic-To-Advance/Practice-Questions/q7.py
@@ -0,0 +1,12 @@
+# WAP to ask the user to enter names of their 3 favorite movies and store them in a list.
+
+a = input("Enter the name of first favourite movie : ")
+b = input("Enter the name of second favourite movie : ")
+c = input("Enter the name of third favourite movie : ")
+
+movies = []
+movies.append(a)
+movies.append(b)
+movies.append(c)
+
+print(movies)
--- Python-Basic-To-Advance/tuples.py
+++ Python-Basic-To-Advance/tuples.py
@@ -10,6 +10,24 @@ print(first_tuple[0])
 
 # first_tuple[0] = 5 --> TypeError: 'tuple' object does not support item assignment because of immutable
 
-empty_tuple = ()
+# empty tuple
+empty_tuple = () # --> vaild tuple
 print(empty_tuple)
 print(type(empty_tuple))
+
+# single value tuple --> (5,) - if we don't use comma python will consider it like integer. 
+single_value_tuple = (5,) # gice comma after value .
+print(single_value_tuple)
+
+# slicing in tuple --> It's work same as list
+tuple_slicing = (1,2,4,3,5,7)
+print(tuple_slicing[1:4])
+
+# Method in tuple
+
+#tup.index(element) --> returns index of first occurrence
+tup = (1,2,4,3,5,7,5)
+print(tup.index(3))
+
+# count --> counts total occurrence : tup.count(element)
+print(tup.count(5))
